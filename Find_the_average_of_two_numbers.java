60d824e2899c2b64a62397e05b3489c19e3328d9
G PREETHI ASHRITHA <119730094+PREETHI-ASHRITHA@users.noreply.github.com>
2023-08-06 18:22:20 +0530

    import java.util.Scanner; public class Average {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         double a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=(a+b)/2;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Average
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        double a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=(a+b)/2;
+        System.out.printf("%.4f",c);
+    }
+}
