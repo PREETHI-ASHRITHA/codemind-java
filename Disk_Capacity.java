96e3cf864102c6b08292449d6bf50d8be76b5f04
G PREETHI ASHRITHA <119730094+PREETHI-ASHRITHA@users.noreply.github.com>
2023-08-07 15:37:02 +0530

    import java.util.Scanner; public class Capacity {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int T,S,B,C;         T=sc.nextInt();         S=sc.nextInt();         B=sc.nextInt();         C=2*T*B*S*512;         System.out.println(C);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Capacity
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int T,S,B,C;
+        T=sc.nextInt();
+        S=sc.nextInt();
+        B=sc.nextInt();
+        C=2*T*B*S*512;
+        System.out.println(C);
+    }
+}
