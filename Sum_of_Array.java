113b7efd449ca69e519743ee12834ff6856b8eb7
G PREETHI ASHRITHA <119730094+PREETHI-ASHRITHA@users.noreply.github.com>
2023-08-19 11:58:02 +0530

    import java.util.Scanner; public class Sum {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n,sum=0;         n=sc.nextInt();         int[] Array= new int[n];         for(int i=0;i<n;i++)         {             Array[i] = sc.nextInt();         }         for(int i=0;i<n;i++)         {             sum+=Array[i];         }         System.out.println(sum);     } }

--- Sum_of_Array.java
+++ Sum_of_Array.java
@@ -4,18 +4,17 @@ public class Sum
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int n;
+        int n,sum=0;
         n=sc.nextInt();
-        int[] intArray = new int[n];
+        int[] Array= new int[n];
         for(int i=0;i<n;i++)
         {
-            intArray[i] = sc.nextInt();
+            Array[i] = sc.nextInt();
         }
-        int sum=0;
-        for(int eachElement: intArray)
+        for(int i=0;i<n;i++)
         {
-            sum +=eachElement;
+            sum+=Array[i];
         }
-        System.out.printf("%d",sum);
+        System.out.println(sum);
     }
 }
